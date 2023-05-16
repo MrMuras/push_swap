d7e1297873c65bd47f18d3751cb0afc181611131
Adam Murawski <amurawsk@c2s3d6.42wolfsburg.de>
2023-05-16 23:02:04 +0200

    swapping

--- swap.c
+++ swap.c
@@ -6,11 +6,37 @@
 /*   By: amurawsk <amurawsk@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 18:27:13 by amurawsk          #+#    #+#             */
-/*   Updated: 2023/05/16 18:28:13 by amurawsk         ###   ########.fr       */
+/*   Updated: 2023/05/16 23:01:55 by amurawsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	sa(t_list *a)
+#include "push_swap.h"
+
+void	sa(t_list **a)
 {
-	
+	void	*temp;
+
+	if (*a == NULL || (*a)->next == NULL)
+		return ;
+	temp = (*a)->content;
+	(*a)->content = (*a)->next->content;
+	(*a)->next->content = temp;
+	ft_printf("sa\n");
+}
+
+void	sb(t_list **b)
+{
+	void	*temp;
+
+	if (*b == NULL || (*b)->next == NULL)
+		return ;
+	temp = (*b)->content;
+	(*b)->content = (*b)->next->content;
+	(*b)->next->content = temp;
+	ft_printf("sb\n");
+}
+
+void	ss(t_list *a)
+{
+	sa()
 }
